667cb950cbbac022a5808cc361121411497f8add
Anderson Simioni Assunçao <anderson.assuncao@parceiro.mrv.com.br>
2022-12-29 17:19:10 -0300

    Documentation BookController

--- Server/WebApi/Controllers/BooksController.cs
+++ Server/WebApi/Controllers/BooksController.cs
@@ -18,6 +18,10 @@ public class BooksController : Controller
     }
     
     [HttpGet("GetCategories")]
+    /// <summary>
+    /// Gets a list of categories from the database
+    /// </summary>
+    /// <returns>A list of categories</returns>
     public IActionResult GetCategories(){
         try
         {
@@ -32,6 +36,10 @@ public class BooksController : Controller
     }
 
     [HttpGet("GetAuthors")]
+    /// <summary>
+    /// Gets a list of authors from the database
+    /// </summary>
+    /// <returns>A list of authors</returns>
     public IActionResult GetAuthors(){
         try
         {
@@ -46,21 +54,33 @@ public class BooksController : Controller
     }
 
     [HttpGet("GetById")]
-    public IActionResult GetById(int bookId){
-        try
-        {
-            var book = _bookStoreContext.GetBookById(bookId);
-            if(book == null) return NotFound("Book not found!");
-            return Ok(book);
-        }
-        catch (System.Exception ex)
-        {
-            _logger.LogError(new EventId(), ex, "Error on get book by ID!");
-            return BadRequest("Internal Server Error");
-        }
+    /// <summary>
+    /// Gets a book from the database by its ID 
+    /// </summary> 
+    /// <param name="bookId">The ID of the book to be retrieved</param> 
+    /// <returns>The book with the specified ID</returns> 
+    public IActionResult GetById(int bookId){ 
+        try 
+        { 
+            var book = _bookStoreContext.GetBookById(bookId); 
+            if(book == null) return NotFound("Book not found!"); 
+            return Ok(book); 
+        } 
+        catch (System.Exception ex) 
+        { 
+            _logger.LogError(new EventId(), ex, "Error on get book by ID!"); 
+            return BadRequest("Internal Server Error"); 
+        } 
     }
 
     [HttpGet("GetByAuthor")]
+    /// <summary>
+    /// Gets a book by author from the book store context
+    /// </summary>
+    /// <param name="author">The author of the book</param>
+    /// <param name="page">The page number of the book</param>
+    /// <param name="pageCount">The page count of the book</param>
+    /// <returns>An IActionResult containing the book or an error message</returns>
     public IActionResult GetByAuthor(string author, int page, int pageCount){
         try
         {
@@ -76,6 +96,13 @@ public class BooksController : Controller
     }
 
     [HttpGet("GetByCategory")]
+    /// <summary>
+    /// Gets a book by category from the book store context
+    /// </summary>
+    /// <param name="category">The category of the book</param>
+    /// <param name="page">The page number of the book</param>
+    /// <param name="pageCount">The page count of the book</param>
+    /// <returns>An IActionResult containing the book or an error message</returns>
     public IActionResult GetByCategory(string category, int page, int pageCount){
         try
         {
@@ -90,22 +117,35 @@ public class BooksController : Controller
         }
     }
 
-    [HttpGet("GetByISBN")]
-    public IActionResult GetByISBN(string ISBN){
-        try
-        {
-            var book = _bookStoreContext.GetBookByISBN(ISBN);
-            if(book == null) return NotFound("Book not found!");
-            return Ok(book);
-        }
-        catch (System.Exception ex)
-        {
-            _logger.LogError(new EventId(), ex, "Error on get book by ISBN!");
-            return BadRequest("Internal Server Error");
-        }
+    [HttpGet("GetByISBN")] 
+    /// <summary> 
+    /// Gets a book by ISBN from the book store context 
+    /// </summary> 
+    /// <param name="ISBN">The ISBN of the book</param> 
+    /// <returns>An IActionResult containing the book or an error message</returns> 
+    public IActionResult GetByISBN(string ISBN){ 
+        try 
+        { 
+            var book = _bookStoreContext.GetBookByISBN(ISBN); 
+            if(book == null) return NotFound("Book not found!"); 
+            return Ok(book); 
+        } 
+        catch (System.Exception ex) 
+        { 
+            _logger.LogError(new EventId(), ex, "Error on get book by ISBN!"); 
+            return BadRequest("Internal Server Error"); 
+        } 
+
     }
 
     [HttpGet("GetByTitle")]
+    /// <summary>
+    /// Gets a book by its title from the database.
+    /// </summary>
+    /// <param name="title">The title of the book.</param>
+    /// <param name="page">The page number.</param>
+    /// <param name="pageCount">The number of books per page.</param>
+    /// <returns>Returns the book if found, otherwise returns a NotFound result.</returns>
     public IActionResult GetByTitle(string title, int page, int pageCount){
         try
         {
@@ -121,6 +161,13 @@ public class BooksController : Controller
     }
 
     [HttpGet("GetByType")]
+    /// <summary>
+    /// Gets a book by its type from the database.
+    /// </summary>
+    /// <param name="type">The type of the book.</param>
+    /// <param name="page">The page number.</param>
+    /// <param name="pageCount">The number of books per page.</param>
+    /// <returns>Returns the book if found, otherwise returns a NotFound result.</returns>
     public IActionResult GetByType(string type, int page, int pageCount){
         try
         {
